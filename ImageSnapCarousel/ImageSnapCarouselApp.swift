edca43721a489d1afd9425a7ea65c1773dbdcce7
onflyer <salkaorama@gmail.com>
2023-09-11 15:11:55 +0200

    Initial Commit

---
+++ ImageSnapCarousel/ImageSnapCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSnapCarouselApp.swift
+//  ImageSnapCarousel
+//
+//  Created by Aleksandar Milidrag on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ImageSnapCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
